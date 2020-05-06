73cf4546e74ff0a88dcf22a51fbc17811a46ef39
chucknezi <jnezia1@gmail.com>
2020-05-06 15:57:57 -0400

    used form data to create a view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,15 @@ export class AppComponent {
   defaultQuestion = 'teacher';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -37,7 +46,12 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.username;
+    this.user.email = this.signupForm.value.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 
 }
